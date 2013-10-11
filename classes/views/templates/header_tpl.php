b4c4da6a33aadcbd930e3fb40ee7fdc3eaeaf1e0
William Slater <williamrslater@gmail.com>
2013-10-10 21:53:48 -0400

    Added forgotten files`

---
+++ classes/views/templates/header_tpl.php
@@ -0,0 +1,13 @@
+<?php
+global $view;
+?>
+
+<html>
+
+    <head>
+        <link rel="stylesheet" type="text/css" href="/css/crat.css">
+        <title><?=$view->getTitle() ?></title>
+    </head>
+
+    <body>
+
